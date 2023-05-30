04a9719a93da15efc62b5c57ea67c3c70d487bdc
jesuschock93 <jesuschock93@gmail.com>
2023-05-30 14:19:38 -0400

    preguntasGeneradorImg

--- src/app.js
+++ src/app.js
@@ -4,58 +4,76 @@ const os = require('os') //SystemInfo
 //* Variables de Entorno
 require("dotenv").config();
 
-//> Dependencies AI
-const {Configuration, OpenAIApi} = require('openai')
-
-const configuration = new Configuration({
-    apiKey: process.env.OPEN_AI_KEY,
-
-});
-
-const openai = new OpenAIApi(configuration);
-
-
-
 //? ejecute Express
 const app = express();
 
 //? use type json
 app.use(express.json());
 
+//> Dependencies AI
+const { Configuration, OpenAIApi } = require('openai')
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 
 
-app.post("/find-complexity", async (req, res) => {
-    try {
-      const { prompt } = req.body;
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `
-                ${prompt}
-        
-                The time complexity of this function is
-                ###
-              `,
-        max_tokens: 64,
-        temperature: 0,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        stop: ["\n"],
-      });
-  
-      return res.status(200).json({
-        success: true,
-        data: response.data
-      });
-    } catch (error) {
-      return res.status(400).json({
-        success: false,
-        error: error.response
-          ? error.response.data
-          : "There was an issue on the server",
-      });
+
+//> Q & A Preguntas y respuestas
+app.post("/chat", async (req, res) => {
+
+    const completion = async () => {
+        const { contentIn } = req.body
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: contentIn }],
+            max_tokens: 70,
+            temperature: 0,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            //stop: ["\n"],
+        });
+
+        return response.data.choices[0].message
     }
-  });
+
+    completion()
+        .then(r => res.status(200).json({
+            success: true,
+            response: `>>>ASSISTANT NATHALY M.<<<: ${r.content} `
+        }))
+        .catch(err => res.status(400).json({
+            success: false,
+            error: err
+        }))
+}
+);
+
+
+//> IMG Generation
+app.post("/generation-img", async ( req, res ) => {
+
+    const imgGenerate = async() => {
+        const { prompt } = req.body
+        const response = await openai.createImage({
+            prompt: prompt,
+            n: 1,
+            size: "512x512"
+        })
+    
+        return response.data.data[0].url;
+    };
+
+    imgGenerate()
+        .then(r => res.status(201).json({
+            url: r
+        }))
+        .catch(err => res.status(400).json({message: err}))
+});
+
 
 
 //> Principal Main PAge
